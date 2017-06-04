70a13465f8235307df36cf7ba6d6e50b17d955a5
Javier Ciangarotti <ciangarotti@gmail.com>
2017-06-04 01:18:55 -0400

    Editing the routes file to adding the new path api/v1

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
-Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+Rails.application.routes.draw do 
+	namespace :api, default:{format: :json} do
+		namespace :v1 do
+		end
+	end 
 end
