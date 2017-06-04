343c27b2709fc9a7a1151b079ac9041b57cf71bb
Javier Ciangarotti <ciangarotti@gmail.com>
2017-06-04 10:10:04 -0400

    Adding the rest of the options to have CRUD

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -4,4 +4,36 @@ class Api::V1::UsersController < ApplicationController
 	def show
 		respond_with User.find(params[:id])
 	end
+
+	# Creating users
+	def create
+		user=User.new(user_params)
+		if user.save
+			render json: user, status: 201
+		else
+			render json: { errors: user.errors}, status: 422
+		end
+	end
+
+	# Updating users
+	def update
+		user = User.find(params[:id])
+		if user.update(user_params)
+			render json: user, status: 200
+		else
+			render json: { errors: user.errors }, status: 422
+		end
+	end
+
+	# Deleting users
+	def destroy
+		user = User.find(params[:id])
+		user.destroy
+		head 204
+	end
+
+	private
+	def user_params
+		params.require(:user).permit(:email, :password, :password_confirmation)
+	end
 end  
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
       #devise_for :users
-      resources :users, :only => [:show, :create]
+      resources :users, :only => [:show, :create, :update, :destroy]
     end
   end  
 end
